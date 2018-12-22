8381713ed78cf69fcb5e16deffc79dc7a545e369
王德强 <1490747123@qq.com>
2018-12-22 14:17:11 +0800

    完成控件基本功能

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Taobaoke;
 
 namespace WpfApp
 {
@@ -23,6 +26,51 @@ namespace WpfApp
         public MainWindow()
         {
             InitializeComponent();
+
+            var webBrowserHelper = new WebBrowserHelper(browser);
+            webBrowserHelper.NewWindow += WebBrowserOnNewWindow; 
+        }
+
+        private void WebBrowserOnNewWindow(object sender, CancelEventArgs e)
+        {
+            dynamic browser = sender;
+            dynamic activeElement = browser.Document.activeElement;
+            var link = activeElement.ToString();
+            // 这儿是在新窗口中打开，如果要在内部打开，改变当前browser的Source就行了
+            browser.Source = new Uri(link);
+            e.Cancel = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void WebBrowser_Loaded(object sender, RoutedEventArgs e)
+        {
+            var b = sender as WebBrowser;
+            if (b == null)
+            {
+                return;
+            }
+            SetWebBrowserSilent(b, true);
+            b.Navigate("https://www.wenku8.net/index.php"); 
+        }
+         
+
+        private void SetWebBrowserSilent(WebBrowser webBrowser, bool silent)
+        {
+            FieldInfo fi = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fi != null)
+            {
+                object browser = fi.GetValue(webBrowser);
+                if (browser != null)
+                    browser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, browser, new object[] { silent });
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var url = browser.Source;
         }
     }
 }
